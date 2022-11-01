181d0b261649773bcc94b7fd858e9d71e0004b89
Arjun V R <arjunvr1610@gmail.com>
2022-11-01 19:40:31 +0530

    Changes in map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,42 +1,65 @@
 import React, { useMemo } from 'react'
-import { GoogleMap, useLoadScript, MarkerF, DirectionsRenderer } from '@react-google-maps/api';
+import {
+  GoogleMap,
+  useLoadScript,
+  MarkerF,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer
+} from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
   height: '410px'
 };
 
-// const calculateRoute = () => {
-//   const directionServices = new google.maps.DirectionsService();
-//   const result = directionServices.route({
-//     origin: { lat: 44, lng: -80 },
-//     destination: { lat: 44, lng: -80.17678 },
-//     travelMode: google.maps.TravelMode.DRIVING
-//   })
-// }
+const generateNodes = (position) => {
+  const nodes = []
+  for (let i = 0; i < 10; i++) {
+    const direction = Math.random() < 0.5 ? -2 : 2;
+    nodes.push({
+      pos: {
+        lat: position.lat + Math.random() / direction,
+        lng: position.lng - Math.random() / direction
+      },
+      label: i.toString()
+    })
+  }
+  return nodes;
+}
 
 const LoadMap = () => {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  const center = useMemo(() => ({ lat: 18.59, lng: 73.7 }), []);
+  const options = useMemo(() => ({
+    disableDefaultUI: true,
+    clickableIcons: false
+  }), []);
+  const image = process.env.PUBLIC_URL + '/warehouse.png'
+
+  const nodes = useMemo(() => generateNodes(center), [center]);
+
   return (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
+      options={options}
       zoom={10}
     >
-      <MarkerF position={{ lat: 44, lng: -80 }} />
-      <MarkerF position={{ lat: 44, lng: -80.124353 }} />
-      <MarkerF position={{ lat: 44, lng: -80.17678 }} />
+      <MarkerF
+        position={center}
+        icon={image}
+      />
+      {nodes.map((node) => <MarkerF label={node.label} key={node.pos.lat} position={node.pos} />)}
     </GoogleMap>
   );
 }
 
 const Map = () => {
-  
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAP_KEY
   });
   if (!isLoaded) return <div>Loading...</div>
-  console.log(process.env.mapApiKey)
   return <LoadMap />
 }
 
